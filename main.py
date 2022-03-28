176f56a459ff472265b99e61de809268fffd0a88
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-03-28 18:22:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+Supermart=Client(
+    "Supermart"
+    bot_token="5001347153:AAHepqJMqzLySGbrfSYeUj2oHVp5fv6sfhw",
+    api_hash="5820bc246505e0ff60af5391d649f9a6",
+    api_id="8406611"
+)
+
+@Supermart.on_message(filters.command("start"))
+async def start_message(bot, message):
+    await message.reply_text("Hi Man 😁")
