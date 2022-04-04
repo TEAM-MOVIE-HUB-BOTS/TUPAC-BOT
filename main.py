7d8350f0a5c2aa0f7dbd2378b73040e25cb03242
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-04-04 07:11:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from pyrogram import Client
+
+
+
+
+Supermart = Client(
+    "Supermart",
+    bot_token="5001347153:AAEvWkC9jGW9Vu9hMckyRT4qlL7hSZuuyPg",
+    api_hash="5820bc246505e0ff60af5391d649f9a6",
+    api_id="8406611",
+    plugins=dict(root="Bot")
+)
+
+
+Supermart.run()
