054d5d8e25280cab9a197ae9cc24795fd3ff2143
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-04-04 07:18:25 +0530

    Update callback.py

--- Bot/callback.py
+++ Bot/callback.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 
 
-@Client.on_callback_Query()
+@Client.on_Callback_Query()
 async def cb(bot, msg):
     if msg.data == "start":
      await msg.message_edit("hi")
