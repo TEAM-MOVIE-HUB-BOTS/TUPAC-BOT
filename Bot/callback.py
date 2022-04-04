17071dc48b5ac32d5cbd03ca53cc7e5547a03e5a
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-04-04 07:37:14 +0530

    Delete callback.py

--- Bot/callback.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery
-
-@Bot.on_Callback_Query()
-async def cb(bot, msg):
-    if msg.data == "start":
-     await msg.message_edit("hi")
