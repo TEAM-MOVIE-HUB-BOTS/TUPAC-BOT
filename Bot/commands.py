b7d73a3ec78cc77a142beee7f03e4f1406017287
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-04-19 17:51:48 +0530

    Update commands.py

--- Bot/commands.py
+++ Bot/commands.py
@@ -1,8 +1,38 @@
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+
+force_channel = "Movie_hub_lite"
+
+Supermart=Client(
+    "Supermart",
+    bot_token="5001347153:AAHepqJMqzLySGbrfSYeUj2oHVp5fv6sfhw",
+    api_hash="5820bc246505e0ff60af5391d649f9a6",
+    api_id="8406611"
+)
+
+@Supermart.on_message(filters.command("start"))
+async def start_message(bot, message):
+    if force_channel:
+        try:
+            user = await bot.get_chat_member(force_channel, message.from_user.id)
+            if user.status == "kicked out":
+                await message.reply_text("I'm sorry ☹️ , you banned")
+                return
+        except UserNotParticipant:
+             await message.reply_text(
+                  text="Your not Subscribe My Channel ✨",
+                  reply_markup=InlineKeyboardMarkup( [[
+                   InlineKeyboardBotton("💖 SUBSCRIBE NOW 💖", url=f"t.me/{force_channel}")
+                   ]]
+                  )
+             )
 
 
-@Client.on_message(filters.command("start"))
-async def start_cmd(bot, msg):
-    await msg.reply_text("👋")
 
 
+
+    await message.reply_text("Hi Man 😁")
+
+
+Supermart.run()
