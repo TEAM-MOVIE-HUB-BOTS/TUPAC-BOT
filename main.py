906236af60c4d8fc494aa0e3e6526c8f43d47587
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-04-29 09:48:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,16 @@ Bot = Client(
     api_hash="5820bc246505e0ff60af5391d649f9a6"
 )
 
+ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+
 DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
 
 START_TEXT = """Hello {},
-I am an under 5MB media or file to telegra.ph link uploader bot.
+Work Not Completed 😫.
 
-Made by @FayasNoushad"""
+Love You ❤️
+
+Made by @Movie_Hub_Bots"""
 
 
 HELP_TEXT = """**About Me**
@@ -150,6 +154,55 @@ async def uploadgif(client, message):
       await msg.edit_text("Something really Happend Wrong...") 
   else:
     await message.reply_text("Size Should Be Less Than 5 mb")
+
+
+
+
+
+@Bot.on_message(Filters.regex(ytregex))
+async def ytdl(_, message):
+    userLastDownloadTime = user_time.get(message.chat.id)
+    try:
+        if userLastDownloadTime > datetime.now():
+            wait_time = round((userLastDownloadTime - datetime.now()).total_seconds() / 60, 2)
+            await message.reply_text(f"`Wait {wait_time} Minutes before next Request`")
+            return
+    except:
+        pass
+
+    url = message.text.strip()
+    await message.reply_chat_action("typing")
+    try:
+        title, thumbnail_url, formats = extractYt(url)
+
+        now = datetime.now()
+        user_time[message.chat.id] = now + \
+                                     timedelta(minutes=youtube_next_fetch)
+
+    except Exception:
+        await message.reply_text("`යූටියුබ් දත්ත ලබා ගැනීමට අසමත් විය ... 😔 \nයූටියුබ් සේවාදායකය අවහිර වී ඇත..\n#දෝෂයකි\n\nFailed To Fetch Youtube Data... 😔 \nPossible Youtube Blocked server ip \n#error`")
+        return
+    buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
+    sentm = await message.reply_text("ලින්කුව සොයමින් 👩‍🔧👩‍🔧\n\nProcessing Youtube Url 👩‍🔧👩‍🔧")
+    try:
+        # Hiruwa
+        img = wget.download(thumbnail_url)
+        im = Image.open(img).convert("RGB")
+        output_directory = os.path.join(os.getcwd(), "downloads", str(message.chat.id))
+        if not os.path.isdir(output_directory):
+            os.makedirs(output_directory)
+        thumb_image_path = f"{output_directory}.jpg"
+        im.save(thumb_image_path,"jpeg")
+        await message.reply_photo(thumb_image_path, caption=title, reply_markup=buttons)
+        await sentm.delete()
+    except Exception as e:
+        print(e)
+        try:
+            thumbnail_url = "https://telegra.ph/file/10bdbaab2d4d163e2affa.jpg"
+            await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
+        except Exception as e:
+            await sentm.edit(
+            f"<code>{e}</code> #Error")
 
 
 Bot.run()
