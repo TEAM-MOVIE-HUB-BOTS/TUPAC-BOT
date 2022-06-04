795d89ebab71ffbb21547833b785dec2abbe9651
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-04 22:44:04 +0530

    Delete commands.py

--- Plugins/commands.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-
-
-
-@Huzain.on_message(filters.command('start') & filters.private)
-async def start(bot, message):
-    await message.reply(
-        f"Hi, {message.chat.first_name} !!\n\n"
-        "My Name Is .Maude Apatow  🤗,  My Work Not Completed ❌ \n Owner By © @Movie_Hub_Lite 😅")
