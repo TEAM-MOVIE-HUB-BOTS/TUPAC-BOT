70be148ad901a61161bd7c9c470a8e212ea96f90
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-04 22:43:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from pyrogram import Client
 
+
+PasswordBOT = Client(
+    "Password BOT",
+    api_hash="5820bc246505e0ff60af5391d649f9a6",
+    api_id="8406611",
+    bot_token="5496896007:AAEQUBtUDtp5A_XTiPnloiceVRPBGVZ8smQ",
+    plugins=dict(root="PasswordBot")
+)
+
+
+PasswordPDFBOT.run()
