287465257e4ddf6bb92fead2b056099fe1202654
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-05 19:30:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pyrogram import Client
 
 
-PasswordBOT = Client(
-    "Password BOT",
+MUSICBOT = Client(
+    "MUSIC BOT",
     api_hash="5820bc246505e0ff60af5391d649f9a6",
     api_id="8406611",
     bot_token="5594929276:AAF6fGIGHNPZNBghjf_ABVxoVS4W1oFDYJg",
-    plugins=dict(root="WEBPDFBOT")
+    plugins=dict(root="MUSICBOT")
 )
 
 
-PasswordBOT.run()
+MUSICBOT.run()
