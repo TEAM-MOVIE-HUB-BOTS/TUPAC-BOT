ec606de692f23ec037236a55fe6498d62a18a4ac
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-05 10:31:30 +0530

    Thanks Samadii ❤️

--- PasswordBot/commands.py
+++ PasswordBot/commands.py
@@ -8,24 +8,19 @@ async def start_cmd(bot, message):
 
 
 @Client.on_message(filters.private & filters.text)
-async def password(bot, message):
-    
-    message = await message.reply_text('`Processing...`')
-    
+async def webtopdf(_, m):
+    url = m.text
+    name = re.sub(r'^\w+://', '', url.lower())
+    name = name.replace('/', '-') + '.pdf'
+    msg = await m.reply("Processing..")
     try:
-        if len(update.text.split()) > 1:
-            keys, limit = update.text.split()[1], int(update.text.split()[0])
-        else:
-            keys = "abcdefghijklmnopqrstuvwxyz"+"1234567890"+"!@#$%^&*()_+".lower()
-            limit = int(update.text)
-    except:
-        await message.edit_text('Something wrong')
-        return
-    
-    if limit > 100 or limit <= 0:
-        text = "Sorry... Failed To Create Password, Because Limit is 1 to 100."
-    else:
-        random_value = "".join(random.sample(password, limit))
-        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @EKBOTZ_UPDATE"
-    
-    await message.edit_text(text, True)
+        await pyppdf.save_pdf(name, url)
+    except PageError:
+        return await msg.edit('URL could not be resolved.')
+    except TimeoutError:
+        return await msg.edit('Timeout.')
+    except NetworkError:
+        return await msg.edit('No access to the network.')
+    await m.reply_document(name)
+    await msg.delete()
+    os.remove(name)
