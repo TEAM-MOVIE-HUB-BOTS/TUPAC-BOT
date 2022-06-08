ee255a059c3fda1308e57de70cbf1ebf53837639
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-08 16:07:01 +0530

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config(object):
+    SESSION_NAME = os.environ.get("SESSION_NAME")
+    API_ID = int(os.environ.get("8406611",))
+    API_HASH = os.environ.get("5820bc246505e0ff60af5391d649f9a6")
+    BOT_TOKEN = os.environ.get("5471316306:AAF3-xjAAwxGnx34YBDI7mniE01Qe7CbcUI")
+    MAX_INLINE_RESULTS = int(os.environ.get("50",))
