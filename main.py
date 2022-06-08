68512e523b2e3bdce09571271a2a0902cf5b81b1
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-08 15:46:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ BOT = Client(
     "BOT",
     api_hash="5820bc246505e0ff60af5391d649f9a6",
     api_id="8406611",
-    bot_token="5594929276:AAF6fGIGHNPZNBghjf_ABVxoVS4W1oFDYJg",
+    bot_token="5471316306:AAF3-xjAAwxGnx34YBDI7mniE01Qe7CbcUI",
     plugins=dict(root="Plugins")
 )
 
