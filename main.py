5fb7a5c0a95380bf6ef59334221b6509945315b0
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-08 08:49:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pyrogram import Client
 
 
-MUSICBOT = Client(
-    "MUSIC BOT",
+BOT = Client(
+    "BOT",
     api_hash="5820bc246505e0ff60af5391d649f9a6",
     api_id="8406611",
     bot_token="5594929276:AAF6fGIGHNPZNBghjf_ABVxoVS4W1oFDYJg",
-    plugins=dict(root="MUSICBOT")
+    plugins=dict(root="Plugins")
 )
 
 
-MUSICBOT.run()
+BOT.run()
