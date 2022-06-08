d91f3e94d68ba19fea85aff2692dc95c16cbf2f8
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-08 20:28:28 +0530

    Delete configs.py

--- configs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config(object):
-    SESSION_NAME = os.environ.get("SESSION_NAME")
-    API_ID = os.environ.get("8406611",)
-    API_HASH = os.environ.get("5820bc246505e0ff60af5391d649f9a6")
-    BOT_TOKEN = os.environ.get("5471316306:AAF3-xjAAwxGnx34YBDI7mniE01Qe7CbcUI")
-    MAX_INLINE_RESULTS = os.environ.get("50",)
