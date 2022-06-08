6e0dd680f81b810c97504deda788eed28cf6a4cb
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-08 08:48:32 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, filters
+
+
+
+@Client.on_message(filters.command("start"))
+async def start_cmd(bot, message):
+    await message.reply(f""Hi, {message.chat.first_name}")
 
