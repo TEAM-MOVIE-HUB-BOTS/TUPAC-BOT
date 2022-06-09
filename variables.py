9ab91ebe44e7e099a4ef6240d679d33efd9e542c
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-09 15:57:50 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,4 +2,7 @@ import re
 import os
 from os import environ
 
+
+id_pattern = re.compile(r'^.\d+$')
+
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '1467649219').split()]
