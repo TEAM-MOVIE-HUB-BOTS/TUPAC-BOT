ab76a31aaea3722f0599c2c8e08d6e668411a6a0
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-10 20:05:21 +0530

    Update and rename variables.py to config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-import re
 import os
-from os import environ
+import logging
 
-
-id_pattern = re.compile(r'^.\d+$')
 
 PICS = os.environ.get("PICS", "https://telegra.ph/file/78e7dd10ae3006381c951.jpg").split()
