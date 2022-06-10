cb9022d244afa174a3c14f96c86c522f455fe184
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-10 20:50:09 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -1,12 +1,13 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import PICS
+from config import PICS, ADMIN
 import random
-
+from DB.db import insert, getid
 
 
 @Client.on_message(filters.command('start') & filters.private)
 async def start_cmd(bot, message):
+    insert(int(message.chat.id))
     await message.reply_chat_action("typing")
     await message.reply_photo(
         photo=random.choice(PICS),
@@ -17,3 +18,25 @@ async def start_cmd(bot, message):
             ]]
             )
         )         
+
+
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
+async def broadcast(bot, message):
+ if (message.reply_to_message):
+   ms = await message.reply_text("Geting All ids from database ...........")
+   ids = getid()
+   tot = len(ids)
+   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
+   for id in ids:
+     try:
+     	await message.reply_to_message.copy(id)
+     except:
+     	pass
+
+
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["users"]))
+async def get_users(client: Client, message: Message):    
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+    ids = getid()
+    tot = len(ids)
+    await msg.edit(f"Total uses = {tot}")
