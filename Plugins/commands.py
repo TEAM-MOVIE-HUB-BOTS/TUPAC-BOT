9ba886b8d6643ed5ce23c4527b72000228d1387f
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-10 20:42:04 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -15,4 +15,4 @@ async def start_cmd(bot, message):
             InlineKeyboardButton("🍁Group🍁", url="https://t.me/A_BOTT"),
             InlineKeyboardButton("🍀Updates🍀", url="https://t.me/B")
             ]]
-            )          
+        )          
