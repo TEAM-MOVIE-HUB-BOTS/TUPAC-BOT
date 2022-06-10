3d3a4bc0222f71ec1e70b622c35909225628e0b6
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-10 20:52:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-import os
+from os import environ
 import logging
+import re
 
+id_pattern = re.compile(r'^.\d+$')
 
-PICS = os.environ.get("PICS", "https://telegra.ph/file/78e7dd10ae3006381c951.jpg").split()
+PICS = environ.get("PICS", "https://telegra.ph/file/78e7dd10ae3006381c951.jpg").split()
+
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', 0).split()]         
