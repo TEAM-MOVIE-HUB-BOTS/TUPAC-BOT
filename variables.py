73f84fc63db8e6043366de5f961fa18b41874bf6
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-10 11:30:01 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -5,7 +5,7 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
-PICS = os.environ.get("PICS", "https://telegra.ph/file/f1e190cc3cf92409ecefb.jpg").split()
+PICS = os.environ.get("PICS", "https://telegra.ph/file/78e7dd10ae3006381c951.jpg").split()
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '900873119').split()]
 
