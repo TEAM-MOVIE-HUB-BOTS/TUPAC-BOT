4566c3912abcd122254b2238fd17de57bdae25b5
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-11 09:56:18 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -12,21 +12,28 @@ async def start_cmd(bot, message):
     await message.reply_photo(
         photo=random.choice(PICS),
         caption=f"Hello {message.from_user.mention}👋🏻 My Name Is TuPc \nI Can Download Muisc From YouTube",
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text='🤔 Help', callback_data='help'), InlineKeyboardButton(text='🤖 About', callback_data='about')], [InlineKeyboardButton(text='Close 🔒', callback_data='close')]])
+        reply_markup = InlineKeyboardMarkup( [[
+            InlineKeyboardButton(text='🤔 Help', callback_data='help'),
+            InlineKeyboardButton(text='🤖 About', callback_data='about')
+            ],[
+            InlineKeyboardButton(text='Close 🔒', callback_data='close')       
+            ]]
+            )
+        )
 
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
-    if (message.reply_to_message):
-      ms = await message.reply_text("Geting All ids from database ...........")
-      ids = getid()
-      tot = len(ids)
-      await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
-      for id in ids:
-        try:
-     	   await message.reply_to_message.copy(id)
-        except:
-     	   pass
+ if (message.reply_to_message):
+   ms = await message.reply_text("Geting All ids from database ...........")
+   ids = getid()
+   tot = len(ids)
+   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
+   for id in ids:
+     try:
+     	await message.reply_to_message.copy(id)
+     except:
+     	pass
 
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["users"]))
