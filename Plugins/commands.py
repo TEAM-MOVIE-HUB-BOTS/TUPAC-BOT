2494dcd8a2e2da5e88c31de8dcf08277380e997f
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-11 09:43:52 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -12,7 +12,7 @@ async def start_cmd(bot, message):
     await message.reply_photo(
         photo=random.choice(PICS),
         caption=f"Hello {message.from_user.mention}👋🏻 My Name Is TuPc \nI Can Download Muisc From YouTube",
-        reply_markup=reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text='🤔 Help', callback_data='help'), InlineKeyboardButton(text='🤖 About', callback_data='about')], [InlineKeyboardButton(text='Close 🔒', callback_data='close')]])
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text='🤔 Help', callback_data='help'), InlineKeyboardButton(text='🤖 About', callback_data='about')], [InlineKeyboardButton(text='Close 🔒', callback_data='close')]])
 
 
 
