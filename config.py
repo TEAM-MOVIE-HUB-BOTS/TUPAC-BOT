5146d6190550eb7b101da0889223eff7238becf2
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-06-11 10:00:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ id_pattern = re.compile(r'^.\d+$')
 
 PICS = environ.get("PICS", "https://telegra.ph/file/78e7dd10ae3006381c951.jpg").split()
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', 0).split()]         
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '1467649219').split()]         
